a1cb9742250fc2d5e83252e530ff4d5852322452
AngelSuarezKea <asuarez@kea.mx>
2022-11-26 00:01:04 -0600

    hello xd

--- Products.jsx
+++ Products.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import PrimaryButton from '../hooks/PrimaryButton';
+import {getProducts} from '../../apiClient/products';
 
 function Products() {
+    const [products, setProducts] = useState([]);
+
+    const getListProducts = async () => {
+        try {
+            const res = await getProducts();
+            setProducts(res);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        getListProducts();
+    }, [])
+    
     return (
-        <div>
-            Hello from products.
+        <div className='p-10'>
+            <span className='text-3xl font-medium mb-4 block'>Submódulo de productos</span>
+            <div>
+                <PrimaryButton>
+                    Hola a todos los productos
+                </PrimaryButton>
+            </div>
+            <div className='w-full mt-6'>
+                {products.map(product => (
+                    <div key={ product.id }>
+                        <span>{product.name}</span>
+                    </div>
+                ))
+                }
+            </div>
         </div>
     )
 }
